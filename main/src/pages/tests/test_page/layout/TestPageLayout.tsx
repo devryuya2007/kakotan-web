7bc7edeebb7b43a64a06eb4dbbea7190657fc597
YamakeRyuya <yamaryu2007@gmail.com>
2025-10-21 12:35:33 +0900

    TestPageLayoutコンポーネントに質問のインデックス管理機能を追加し、選択肢のクリック処理を実装。選択肢のシャッフル処理を維持しつつ、状態管理を強化。

--- main/src/pages/tests/test_page/layout/TestPageLayout.tsx
+++ main/src/pages/tests/test_page/layout/TestPageLayout.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { type QuizQuestion } from "../../../../data/vocabLoader";
 
 type TestPageLayoutProps = {
@@ -7,12 +7,33 @@ type TestPageLayoutProps = {
 };
 
 export default function TestPageLayout({ questions }: TestPageLayoutProps) {
-  const question = questions[0];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  if (!questions[currentIndex]) return null;
+  const question = questions[currentIndex];
+  const answerChoice = question.choices[question.answerIndex];
+
   if (!question) return null;
 
   const shuffled = useMemo(() => {
     return [...question.choices].sort(() => Math.random() - 0.5);
   }, [question]);
+
+  const [selectedChoice, setSelectedChoice] = useState<string | null>(null);
+  const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
+
+  function handleClick(choice: string) {
+    setSelectedChoice(choice);
+
+    if (choice === answerChoice) {
+      setIsCorrect(true);
+
+      setTimeout(() => {
+        setCurrentIndex((i) => i + 1);
+      }, 500);
+    }
+
+    if (choice !== answerChoice) setIsCorrect(false);
+  }
 
   return (
     <div className="rounded-2xl bg-gradient-to-b from-[#b8860b] to-[#f2c97d] p-[2px]">
@@ -23,9 +44,9 @@ export default function TestPageLayout({ questions }: TestPageLayoutProps) {
         <div className="grid grid-cols-2 gap-3 text-md text-white/80">
           {shuffled.map((choice, index) => (
             <button
+              onClick={() => handleClick(choice)}
               key={index}
-              className="rounded-lg border border-white/10 bg-white/5 px-4 py-3 text-left transition-transform hover:-translate-y-1 hover:border-[#f2c97d] hover:bg-white/10"
-            >
+              className="rounded-lg border border-white/10 bg-white/5 px-4 py-3 text-left transition-transform hover:-translate-y-1 hover:border-[#f2c97d] hover:bg-white/10">
               {choice}
             </button>
           ))}
